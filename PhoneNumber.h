2fbc42e3b0184679f4d8b5c18546b6bdb8f22f35
Shames <assamialomarshames@gmail.com>
2018-11-14 03:24:42 +0600

    built and added all the fuction which is need to PhoneNumber.h file done

--- PhoneNumber.h
+++ PhoneNumber.h
@@ -1,6 +1,23 @@
 #ifndef PHONENUMBER_H_INCLUDED
 #define PHONENUMBER_H_INCLUDED
+#include <iostream>
 
+using namespace std;
 
+//template<class ItemType>
+class PhoneNumber
+{
+public:
+    PhoneNumber();
+    PhoneNumber(string oparator);
+    void set_phone(string phone);
+    void set_oparator(string oparator);
+    string get_oparator();
+    string get_phone();
+
+private:
+    string phoneNumber;
+    string oparator;
+};
 
 #endif // PHONENUMBER_H_INCLUDED
